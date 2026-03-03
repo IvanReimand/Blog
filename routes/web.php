0be84347370eab5003b609aa285d44e5a838bc6f
IvanR <Ivan.Reimand@tptlive.ee>
2026-03-03 23:43:30 +0200

    posts

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [PublicController::class, 'index'])->name('home');
+Route::get('/post/{post}', [PublicController::class, 'post'])->name('post');
